32e6b10010e7665ab562ec5268ca5e622bdc2645
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-07-15 14:58:53 +0200

    Fifth one clear ... i think

---
+++ src/main/java/se/lexicon/vxo/App.java
@@ -0,0 +1,17 @@
+package se.lexicon.vxo;
+
+
+import se.lexicon.vxo.model.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class App
+{
+    public static void main( String[] args ) {
+
+
+    }
+}
